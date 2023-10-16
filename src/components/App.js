4e35bb2cfba61dac7d8f5a828fce7b437bb61e49
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-10-16 00:08:43 -0700

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
-// create your App component here
+import { useEffect, useState } from "react";
+import React from "react";
+
+const URL = "https://dog.ceo/api/breeds/image/random";
+
+function App() {
+  const [image, setImage] = useState("");
+
+  useEffect(() => {
+    fetch(URL)
+      .then((resp) => resp.json())
+      .then((dogImgData) => setImage(dogImgData.message))
+      .catch((Error) => console.log(Error));
+  }, []);
+
+  const img = () => {
+    if (!image) return <p>Loading...</p>;
+
+    return (
+      <div>
+        <img src={image} alt="A Random Dog" />
+      </div>
+    );
+  };
+
+  return <div>{img()}</div>;
+}
+
+export default App;
